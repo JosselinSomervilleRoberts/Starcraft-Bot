afdee640a7e2ea03e51b863f30e6d69e8593649d
LAPTOP-LK82EF7O\mpilc <mpilcer@hotmail.fr>
2022-03-02 09:36:43 +0100

    resolved bugs

--- windows/c++/src/BuildTask.cpp
+++ windows/c++/src/BuildTask.cpp
@@ -31,14 +31,13 @@ void BuildTask::update(bool& enoughMinerals, bool& enoughGas) {
     const auto pylonType = BWAPI::UnitTypes::Protoss_Pylon;
     if (m_toBuild.getID() != BWAPI::UnitTypes::Enum::None) {
 
-<<<<<<< HEAD
         switch (m_state) {
         case State::initialize:
             // TODO: Make checks and stuff...
             m_state = State::reserveResources; // go to next state
             break;
         case State::reserveResources:
-            if (m_manager->reserveRessources(m_toBuild.mineralPrice(), m_toBuild.gasPrice()))
+            if (m_manager->reserveRessources(m_toBuild.mineralPrice(), m_toBuild.gasPrice(), enoughMinerals, enoughGas))
                 m_state = State::acquireWorker; // go to next state
             break;
         case State::acquireWorker:
@@ -55,7 +54,6 @@ void BuildTask::update(bool& enoughMinerals, bool& enoughGas) {
             break;
 
         case State::moveToPosition: 
-            m_manager->releaseRessources(m_toBuild.mineralPrice(), m_toBuild.gasPrice());
             if (m_toBuild == refineryType) m_manager->setRefineryState(BuildingState::CONSTRUCTING);
             
             if (!m_allocatedBuildPosition) {
@@ -96,76 +94,14 @@ void BuildTask::update(bool& enoughMinerals, bool& enoughGas) {
                     m_state = State::moveToPosition; // go back and try again
                 }
             }
-            else if (true) {
+            else {
                 // Train unit
                 const BWAPI::Unit trainBuilding = Tools::GetUnitOfType(m_toBuild.whatBuilds().first);
                 
                 if (trainBuilding && trainBuilding->train(m_toBuild)) {
                     m_manager->releaseRessources(m_toBuild.mineralPrice(), m_toBuild.gasPrice());
-=======
-    switch (m_state) {
-    case State::initialize:
-        // TODO: Make checks and stuff...
-        m_state = State::reserveResources; // go to next state
-        break;
-    case State::reserveResources:
-        if (m_manager->reserveRessources(m_toBuild.mineralPrice(), m_toBuild.gasPrice(), enoughMinerals, enoughGas))
-            m_state = State::acquireWorker; // go to next state
-        break;
-    case State::acquireWorker:
-        if (m_toBuild.isBuilding()) {
-            m_worker = m_manager->acquireWorker();// m_toBuild.whatBuilds().first);// , Position(m_position));
-            if (m_worker != nullptr) {
-                // Go to next state
-                m_state = State::moveToPosition;
-            }
-        }
-        else
-            m_state = State::startBuild;
-        break;
-
-    case State::moveToPosition: {
-        if (m_toBuild == refineryType) m_manager->setRefineryState(BuildingState::CONSTRUCTING);
-        if (!m_allocatedBuildPosition) {
-            m_buildPosition =
-                m_exactPosition ? m_position : BWAPI::Broodwar->getBuildLocation(m_toBuild, m_position);
-            m_allocatedBuildPosition = true;
-        }
-        assert(m_worker != nullptr);
-        const BWAPI::Position movePosition =
-            BWAPI::Position(m_buildPosition) + BWAPI::Position(m_toBuild.tileSize()) / 2;
-
-        // DEBUG
-        BWAPI::Broodwar->registerEvent([worker = m_worker, movePosition](BWAPI::Game*) {
-            BWAPI::Broodwar->drawLineMap(worker->getPosition(), movePosition, BWAPI::Colors::Purple);
-            BWAPI::Broodwar->drawTextMap(worker->getPosition(), "Distance: %d",
-                worker->getDistance(movePosition));
-            },
-            nullptr, BWAPI::Broodwar->getLatencyFrames());
-
-        if (m_worker->getOrder() != BWAPI::Orders::Move ||
-            m_worker->getOrderTargetPosition() != movePosition)
-            m_worker->move(movePosition);
-        if (m_worker->getDistance(movePosition) < 100) // TODO!
-            m_state = State::startBuild;               // go to next state
-        break;
-    }
-    case State::startBuild:
-        if (m_toBuild.isBuilding()) {
-            // Construct building
-            if (BWAPI::Broodwar->canBuildHere(m_buildPosition, m_toBuild, m_worker)) {
-                if (m_worker->build(m_toBuild, m_buildPosition))
->>>>>>> origin/main
                     m_state = State::waitForUnit; // go to next state
                 }
-            }
-            else {
-                
-                /*auto units = BWAPI::PlayerInterface::getUnits();
-                for (auto unit : units) {
-                    if(unit.canUpgrade(m_toUpgrade))
-
-                }*/
             }
             break;
         case State::waitForUnit:
@@ -176,7 +112,9 @@ void BuildTask::update(bool& enoughMinerals, bool& enoughGas) {
         case State::building:
             if (m_toBuild == refineryType) m_manager->setRefineryState(BuildingState::CONSTRUCTED);
             if (m_toBuild.isBuilding()) {
+                m_manager->releaseRessources(m_toBuild.mineralPrice(), m_toBuild.gasPrice());
                 m_manager->releaseWorker(m_worker);
+
             }
             m_state = State::finalize; // go to next state
             break;
@@ -194,46 +132,29 @@ void BuildTask::update(bool& enoughMinerals, bool& enoughGas) {
             m_state = State::reserveResources; // go to next state
             break;
         case State::reserveResources:
-            if (m_manager->reserveRessources(m_toUpgrade.mineralPrice(), m_toUpgrade.gasPrice()))
+            if (m_manager->reserveRessources(m_toUpgrade.mineralPrice(), m_toUpgrade.gasPrice(), enoughMinerals, enoughGas))
                 m_state = State::startBuild; // go to next state
             break;
 
-        case State::startBuild:
-            if (true) {
-                const BWAPI::Unit upgradeBuilding = Tools::GetUnitOfType(m_toUpgrade.whatUpgrades());
-                if (upgradeBuilding && !upgradeBuilding->isResearching() && !upgradeBuilding->isUpgrading()) {
-                    if (upgradeBuilding->upgrade(m_toUpgrade)) {
-                        m_manager->releaseRessources(m_toUpgrade.mineralPrice(), m_toUpgrade.gasPrice());
-                        m_state = State::finalize;
-                    }
+        case State::startBuild: {
+
+            const BWAPI::Unit upgradeBuilding = Tools::GetUnitOfType(m_toUpgrade.whatUpgrades());
+            if (upgradeBuilding && !upgradeBuilding->isResearching() && !upgradeBuilding->isUpgrading()) {
+                if (upgradeBuilding->upgrade(m_toUpgrade)) {
+                    m_manager->releaseRessources(m_toUpgrade.mineralPrice(), m_toUpgrade.gasPrice());
+                    m_state = State::finalize;
                 }
-                // Train unit
-                /* auto units = BWAPI::PlayerInterface::getUnits();
-                for (auto unit : units) {
-                    if (unit.canUpgrade(m_toUpgrade))
-                        unit->upgrade(m_toUpgrade);
-
-                }*/
-            
-            }
-            else {
-                /*auto units = BWAPI::PlayerInterface::getUnits();
-                for (auto unit : units) {
-                    if(unit.canUpgrade(m_toUpgrade))
-
-                }*/
             }
             break;
+        }
+             
         case State::waitForUnit:
             if (m_buildingUnit != nullptr) {
                 m_state = State::building; // go to next state
             }
             break;
         case State::building:
-            if (m_toBuild == refineryType) m_manager->setRefineryState(BuildingState::CONSTRUCTED);
-            if (m_toBuild.isBuilding()) {
-                m_manager->releaseWorker(m_worker);
-            }
+            
             m_state = State::finalize; // go to next state
             break;
         case State::finalize:
@@ -242,45 +163,31 @@ void BuildTask::update(bool& enoughMinerals, bool& enoughGas) {
         default:
             throw std::logic_error("Unknown BuildTask::State!");
         }
-<<<<<<< HEAD
     }
     else { // If it is an upgrade
         switch (m_state) {
-            case State::initialize:
-                // TODO: Make checks and stuff...
-                m_state = State::reserveResources; // go to next state
-                break;
-            case State::reserveResources:
-                if (m_manager->reserveRessources(m_techUpgrade.mineralPrice(), m_techUpgrade.gasPrice()))
-                    m_state = State::startBuild; // go to next state
-                break;
+        case State::initialize:
+            // TODO: Make checks and stuff...
+            m_state = State::reserveResources; // go to next state
+            break;
+        case State::reserveResources:
+            if (m_manager->reserveRessources(m_techUpgrade.mineralPrice(), m_techUpgrade.gasPrice(), enoughMinerals, enoughGas))
+                m_state = State::startBuild; // go to next state
+            break;
 
-            case State::startBuild:
-                if (true) {
-                    const BWAPI::Unit upgradeBuilding = Tools::GetUnitOfType(m_techUpgrade.whatResearches());
-                    if (upgradeBuilding && !upgradeBuilding->isResearching() && !upgradeBuilding->isUpgrading()) {
-                        if (upgradeBuilding->research(m_techUpgrade)) {
-                            m_manager->releaseRessources(m_techUpgrade.mineralPrice(), m_techUpgrade.gasPrice());
-                            m_state = State::finalize;
-                        }
-                    }
-                    // Train unit
-                    /* auto units = BWAPI::PlayerInterface::getUnits();
-                    for (auto unit : units) {
-                        if (unit.canUpgrade(m_toUpgrade))
-                            unit->upgrade(m_toUpgrade);
-
-                    }*/
-
+        case State::startBuild:
+            {
+            const BWAPI::Unit upgradeBuilding = Tools::GetUnitOfType(m_techUpgrade.whatResearches());
+            if (upgradeBuilding && !upgradeBuilding->isResearching() && !upgradeBuilding->isUpgrading()) {
+                if (upgradeBuilding->research(m_techUpgrade)) {
+                    m_manager->releaseRessources(m_techUpgrade.mineralPrice(), m_techUpgrade.gasPrice());
+                    m_state = State::finalize;
                 }
-                else {
-                    /*auto units = BWAPI::PlayerInterface::getUnits();
-                    for (auto unit : units) {
-                        if(unit.canUpgrade(m_toUpgrade))
+            }
 
-                    }*/
-                }
-                break;
+            break;
+            }
+               
             case State::waitForUnit:
                 if (m_buildingUnit != nullptr) {
                     m_state = State::building; // go to next state
@@ -295,23 +202,6 @@ void BuildTask::update(bool& enoughMinerals, bool& enoughGas) {
             default:
                 throw std::logic_error("Unknown BuildTask::State!");
         }
-=======
-        break;
-    case State::building:
-        if (m_toBuild == refineryType) m_manager->setRefineryState(BuildingState::CONSTRUCTED);
-        if (m_toBuild.isBuilding()) {
-            m_manager->releaseRessources(m_toBuild.mineralPrice(), m_toBuild.gasPrice());
-            m_manager->releaseWorker(m_worker);
-        }
-        m_state = State::finalize; // go to next state
-        break;
-    case State::finalize:
-        m_manager->releaseRessources(m_toBuild.mineralPrice(), m_toBuild.gasPrice());
-        // At this state, this build task can be removed from the queue.
-        break;
-    default:
-        throw std::logic_error("Unknown BuildTask::State!");
->>>>>>> origin/main
     }
 }
 
